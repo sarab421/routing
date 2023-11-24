af00f5e5f58941ce483f24e95da827d9c49a99af
Sarab <darsarab421@gmail.com>
2023-11-24 09:51:40 +0500

    single Page Routing ok but can't see anything on that single page

---
+++ src/books.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import SinglePage from "./components/singlePage";
+import { Link } from "react-router-dom";
+
+const Books = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then(function (response) {
+        setPosts(response.data);
+        setLoading(false);
+      })
+      .catch(function (error) {
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
+
+  return (
+    <div>
+      {posts.slice(0, 5).map((item) => (
+        <Link to={`/books/singlePage:${item.id}`} key={item.id}>
+          <SinglePage title={item.title} body={item.body} />
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default Books;
---
+++ src/components/singlePage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SinglePage = (props) => {
+  return (
+    <>
+    <h1>{props.title}</h1>
+    <p>{props.body}</p>
+    <hr />
+    <hr />
+    <hr />
+    </>
+  )
+}
+
+export default SinglePage
